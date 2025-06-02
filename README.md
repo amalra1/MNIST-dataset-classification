b5e4ad6d5a55377306521020ece1a2891ec32b88
fns21 <fabinhosilva7@outlook.com>
2025-06-02 01:14:25 -0300

    adicionando testes

--- README.md
+++ README.md
@@ -1,1 +1,41 @@
-# Classificação dos números do dataset MNIST com KNN e classificadores lineares
+## Descrição
+Classificação dos números do dataset MNIST com KNN e classificadores lineares
+
+## Pré-requisitos
+Certifique-se de ter o seguinte instalado em seu sistema:
+- Python 3.10 ou superior
+- `pip` para gerenciar pacotes Python
+
+## Configuração do Ambiente
+
+1. **Clone o repositório**:
+   ```sh
+   git clone <URL_DO_REPOSITORIO>
+   cd MNIST-dataset-classification
+    ```
+2. **Crie o ambiente virtual**:
+   ```sh
+   python3 -m venv venv
+   ```
+
+3. **Ative o ambiente virtual**:
+    - No Windows:
+      ```sh
+      venv\Scripts\activate
+      ```
+    - No Linux/Mac:
+      ```sh
+      source venv/bin/activate
+      ```
+
+4. **Instale as dependências**:
+    ```sh
+    pip install -r requirements.txt
+    ```
+
+## Uso
+Para executar o script, use o seguinte comando:
+
+```sh
+python3 mnist_classification.py
+```
--- mnist_classification.py
+++ mnist_classification.py
@@ -1,3 +1,4 @@
+import os
 import numpy as np
 from sklearn.datasets import fetch_openml
 from sklearn.model_selection import train_test_split
@@ -19,13 +20,13 @@ def TVT_splitter(x, y, train_ratio, validation_ratio, test_ratio):
 
     return x_train, x_val, x_test, y_train, y_val, y_test
 
-def dimensionality_reduction(X_trainval, x_test_scaled, n_components):
+def dimensionality_reduction(x_train, x_test_scaled, n_components):
 
     pca = PCA(n_components)
-    X_trainval_pca = pca.fit_transform(X_trainval)
-    X_test_pca = pca.transform(x_test_scaled)
+    x_train_pca = pca.fit_transform(x_train)
+    x_test_pca = pca.transform(x_test_scaled)
 
-    return X_trainval_pca, X_test_pca
+    return x_train_pca, x_test_pca
 
 # Carregar dataset
 mnist = fetch_openml('mnist_784', version=1, as_frame=False)
@@ -41,51 +42,71 @@ x_val_scaled = scaler.transform(x_val)
 x_test_scaled = scaler.transform(x_test)
 
 # Grids de hiperparâmetros
-n_components_grid = [20, 40, 60, 80, 100, 200, x.shape[1]/2]
+n_components_grid = [20, 40, 60, 80, 100]
 n_neighbors_grid = [3, 5, 7, 9]
 max_iter_grid = [500, 1000, 2000]
-metrics_grid = ['euclidean', 'manhattan']
 
 # 🔥 Cache de PCA para não recalcular
 pca_cache = {}
 for n_components in n_components_grid:
-    pca = PCA(n_components=n_components)
-    X_train_pca = pca.fit_transform(x_train_scaled)
-    X_val_pca = pca.transform(x_val_scaled)
-    pca_cache[n_components] = (X_train_pca, X_val_pca, pca)
+    pca_cache[n_components] = dimensionality_reduction(x_train_scaled, x_val_scaled, n_components)
 
-# Otimização dos hiperparâmetros do kNN usando os dados de validação
-best_knn_acc = 0
-best_knn_params = {}
-print("choosing hyperparameters for KNN...")
-for n_components in n_components_grid:
-    X_train_pca, X_val_pca, _ = pca_cache[n_components]
-    for n_neighbors in n_neighbors_grid:
-        for metric in metrics_grid:
-            knn = KNeighborsClassifier(n_neighbors=n_neighbors, metric=metric, n_jobs=-1)
-            knn.fit(X_train_pca, y_train)
-            y_pred_val = knn.predict(X_val_pca)
-            acc = accuracy_score(y_val, y_pred_val)
-            if acc > best_knn_acc:
-                best_knn_acc = acc
-                best_knn_params = {
-                    'n_components': n_components,
-                    'n_neighbors': n_neighbors,
-                    'metric': metric
-                }
+# Otimização dos hiperparâmetros do kNN com distancia euclidiana usando os dados de validação
+best_knn_euc_acc = 0
+best_knn_euc_params = {}
+print("Choosing hyperparameters for KNN Euclidean...")
+for i, n_components in enumerate(n_components_grid):
+    print(f"Component: {i+1}/{len(n_components_grid)}, n_components: {n_components}")
+    X_train_pca, X_val_pca = pca_cache[n_components]
+    for j, n_neighbors in enumerate(n_neighbors_grid):
+        print(f"  Neighbors: {j+1}/{len(n_neighbors_grid)}, n_neighbors: {n_neighbors}")
+        knn = KNeighborsClassifier(n_neighbors=n_neighbors, metric='euclidean', n_jobs=-1)
+        knn.fit(X_train_pca, y_train)
+        y_pred_val = knn.predict(X_val_pca)
+        acc = accuracy_score(y_val, y_pred_val)
+        if acc > best_knn_euc_acc:
+            best_knn_euc_acc = acc
+            best_knn_euc_params = {
+                'n_components': n_components,
+                'n_neighbors': n_neighbors,
+            }
 
-print(f"Best kNN params: {best_knn_params}, Validation Accuracy: {best_knn_acc:.4f}")
+print(f"Best kNN euclidian params: {best_knn_euc_params}, Validation Accuracy: {best_knn_euc_acc:.4f}")
+
+# Otimização dos hiperparâmetros do kNN com distancia de manhattan usando os dados de validação
+best_knn_man_acc = 0
+best_knn_man_params = {}
+print("Choosing hyperparameters for KNN Manhattan...")
+for i, n_components in enumerate(n_components_grid):
+    print(f"Component: {i+1}/{len(n_components_grid)}, n_components: {n_components}")
+    X_train_pca, X_val_pca = pca_cache[n_components]
+    for j, n_neighbors in enumerate(n_neighbors_grid):
+        print(f"  Neighbors: {j+1}/{len(n_neighbors_grid)}, n_neighbors: {n_neighbors}")
+        knn = KNeighborsClassifier(n_neighbors=n_neighbors, metric='manhattan', n_jobs=-1)
+        knn.fit(X_train_pca, y_train)
+        y_pred_val = knn.predict(X_val_pca)
+        acc = accuracy_score(y_val, y_pred_val)
+        if acc > best_knn_man_acc:
+            best_knn_man_acc = acc
+            best_knn_man_params = {
+                'n_components': n_components,
+                'n_neighbors': n_neighbors,
+            }
+
+print(f"Best kNN manhattan params: {best_knn_man_params}, Validation Accuracy: {best_knn_man_acc:.4f}")
 
 # Otimização dos hiperparâmetros da Regressão Logística usando os dados de validação
 best_log_acc = 0
 best_log_params = {}
-print("choosing hyperparameters for Logistic Regression...")
-for n_components in n_components_grid:
-    X_train_pca, X_val_pca, _ = pca_cache[n_components]
-    for max_iter in max_iter_grid:
-        clf = LogisticRegression(max_iter=max_iter, n_jobs=-1)
-        clf.fit(X_train_pca, y_train)
-        y_pred_val = clf.predict(X_val_pca)
+print("Choosing hyperparameters for Logistic Regression...")
+for i, n_components in enumerate(n_components_grid):
+    print(f"Component: {i+1}/{len(n_components_grid)}, n_components: {n_components}")
+    X_train_pca, X_val_pca = pca_cache[n_components]
+    for j, max_iter in enumerate(max_iter_grid):
+        print(f"  Iterations: {j+1}/{len(max_iter_grid)}, max_iter: {max_iter}")
+        lr = LogisticRegression(max_iter=max_iter, n_jobs=-1)
+        lr.fit(X_train_pca, y_train)
+        y_pred_val = lr.predict(X_val_pca)
         acc = accuracy_score(y_val, y_pred_val)
         if acc > best_log_acc:
             best_log_acc = acc
@@ -99,9 +120,10 @@ print(f"Best Logistic Regression params: {best_log_params}, Validation Accuracy:
 # Otimização dos hiperparâmetros Naive Bayes usando dos dados de validação
 best_nb_acc = 0
 best_nb_params = {}
-print("choosing hyperparameters for Naive Bayes...")
-for n_components in n_components_grid:
-    X_train_pca, X_val_pca, _ = pca_cache[n_components]
+print("Choosing hyperparameters for Naive Bayes...")
+for i, n_components in enumerate(n_components_grid):
+    print(f"Component: {i+1}/{len(n_components_grid)}, n_components: {n_components}")
+    X_train_pca, X_val_pca = pca_cache[n_components]
     nb = GaussianNB()
     nb.fit(X_train_pca, y_train)
     y_pred_val = nb.predict(X_val_pca)
@@ -112,29 +134,62 @@ for n_components in n_components_grid:
             'n_components': n_components
         }
 
-print(f"Best Naive Bayes params: {best_nb_params}, Validation Accuracy: {best_nb_acc:.4f}")
+print(f"Best Naive Bayes params: {best_nb_params}, Validation Accuracy: {best_nb_acc:.4f}\n")
 
 # Unir treino + validação para teste final
 X_trainval = np.vstack([x_train_scaled, x_val_scaled])
 y_trainval = np.concatenate([y_train, y_val])
+best_log_params['max_iter'] *= int(TEST_RATIO / VALIDATION_RATIO)  # Ajustar max_iter para o tamanho do conjunto de treino + validação
 
-# kNN
-X_trainval_pca, X_test_pca = dimensionality_reduction(X_trainval, x_test_scaled, best_knn_params['n_components'])
-knn = KNeighborsClassifier(n_neighbors=best_knn_params['n_neighbors'], metric=best_knn_params['metric'], n_jobs=-1)
+# Teste final com os melhores parâmetros encontrados, aqui as imagens de teste são classificadas
+
+# kNN euclidean
+X_trainval_pca, X_test_pca = dimensionality_reduction(X_trainval, x_test_scaled, best_knn_euc_params['n_components'])
+knn = KNeighborsClassifier(n_neighbors=best_knn_euc_params['n_neighbors'], metric='euclidean', n_jobs=-1)
 knn.fit(X_trainval_pca, y_trainval)
-y_pred_knn = knn.predict(X_test_pca)
-print("Test Accuracy kNN:", accuracy_score(y_test, y_pred_knn))
+y_pred_knn_euc = knn.predict(X_test_pca)
+print("Test Accuracy kNN euclidean:", accuracy_score(y_test, y_pred_knn_euc))
+
+# kNN manhattan
+X_trainval_pca, X_test_pca = dimensionality_reduction(X_trainval, x_test_scaled, best_knn_man_params['n_components'])
+knn = KNeighborsClassifier(n_neighbors=best_knn_man_params['n_neighbors'], metric='manhattan', n_jobs=-1)
+knn.fit(X_trainval_pca, y_trainval)
+y_pred_knn_man = knn.predict(X_test_pca)
+print("Test Accuracy kNN manhattan:", accuracy_score(y_test, y_pred_knn_man))
 
 # Logistic Regression
 X_trainval_pca, X_test_pca = dimensionality_reduction(X_trainval, x_test_scaled, best_log_params['n_components'])
-clf = LogisticRegression(max_iter=best_log_params['max_iter'], n_jobs=-1)
-clf.fit(X_trainval_pca, y_trainval)
-y_pred_log = clf.predict(X_test_pca)
+lr = LogisticRegression(max_iter=best_log_params['max_iter'], n_jobs=-1)
+lr.fit(X_trainval_pca, y_trainval)
+y_pred_log = lr.predict(X_test_pca)
 print("Test Accuracy Logistic Regression:", accuracy_score(y_test, y_pred_log))
 
 # Naive Bayes
-X_trainval_pca, X_test_pca, _ = dimensionality_reduction(X_trainval, x_test_scaled, best_nb_params['n_components'])
+X_trainval_pca, X_test_pca = dimensionality_reduction(X_trainval, x_test_scaled, best_nb_params['n_components'])
 nb = GaussianNB()
 nb.fit(X_trainval_pca, y_trainval)
 y_pred_nb = nb.predict(X_test_pca)
 print("Test Accuracy Naive Bayes:", accuracy_score(y_test, y_pred_nb))
+
+# Resultados dos testes
+results = []
+results.append(f"Best kNN Euclidean params: {best_knn_euc_params}, Validation Accuracy: {best_knn_euc_acc:.4f}")
+results.append(f"Best kNN Manhattan params: {best_knn_man_params}, Validation Accuracy: {best_knn_man_acc:.4f}")
+results.append(f"Best Logistic Regression params: {best_log_params}, Validation Accuracy: {best_log_acc:.4f}")
+results.append(f"Best Naive Bayes params: {best_nb_params}, Validation Accuracy: {best_nb_acc:.4f}\n")
+results.append(f"Test Accuracy kNN Euclidean: {accuracy_score(y_test, y_pred_knn_euc):.4f}")
+results.append(f"Test Accuracy kNN Manhattan: {accuracy_score(y_test, y_pred_knn_man):.4f}")
+results.append(f"Test Accuracy Logistic Regression: {accuracy_score(y_test, y_pred_log):.4f}")
+results.append(f"Test Accuracy Naive Bayes: {accuracy_score(y_test, y_pred_nb):.4f}")
+
+# Nome do arquivo
+output_dir = "tests"
+os.makedirs(output_dir, exist_ok=True)
+filename = os.path.join(output_dir, f"TVT_{int(TRAIN_RATIO*100)}_{int(VALIDATION_RATIO*100)}_{int(TEST_RATIO*100)}_log.txt")
+
+# Salvar resultados no arquivo
+with open(filename, "w") as f:
+    f.write("Resultados dos Testes :\n")
+    f.write(f"TVT: {TRAIN_RATIO*100:.0f}% - {VALIDATION_RATIO*100:.0f}% - {TEST_RATIO*100:.0f}%\n\n")
+    for line in results:
+        f.write(line + "\n")
